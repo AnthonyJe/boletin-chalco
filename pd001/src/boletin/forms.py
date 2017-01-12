143927265859824344b59eb00b743637749b7831
Antonio de Jesús RH <jesus_omega97@hotmail.com>
2017-01-12 01:03:42 -0600

    Primer commit, con dos appsweb

---
+++ pd001/src/boletin/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from .models import Registrado
+
+class RegModelForm(forms.ModelForm):
+	class Meta:
+		model = Registrado
+		fields = ["email", "nombre"]
+
+	def clean_email(self): 
+		email = self.cleaned_data.get("email")
+		email_base, proveedor = email.split ("@")
+		dominio, extension = proveedor.split(".") 
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza una dirección válida")
+		return email
+
+
+class ContactForm(forms.Form):
+	nombre = forms.CharField(required=False, max_length=100)
+	email = forms.EmailField()
+	mensaje = forms.CharField(widget=forms.Textarea)
---
+++ pd001/src/boletin/views.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render
+
+from .forms import  RegModelForm, ContactForm
+from .models import Registrado
+
+# Create your views here.
+def inicio(request):
+	titulo = "Disfruta del site"
+	if request.user.is_authenticated():
+		titulo = "%s" %(request.user) 
+	form = RegModelForm(request.POST or None)
+	context = {
+		"el_titulo": titulo,
+		"el_form": form,
+	}
+	if form.is_valid():
+		instance = form.save(commit=False)
+		if not instance.nombre:
+			instance.nombre="PERSONA"
+		instance.save()
+
+		context = {
+			"el_titulo": "Gracias %s!" %(instance.nombre)
+		}
+		print (instance)
+		print (instance.timestamp)
+		"""
+		form_data = form.cleaned_data
+		mail = form_data.get('email')
+		name = form_data.get('nombre')
+		obj = Registrado.objects.create(email=mail, nombre=name)
+		"""
+	
+	return render(request, "inicio.html", context)
+
+def contact(request):
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		email = form.cleaned_data.get("email")
+		mensaje = form.cleaned_data.get("mensaje")
+		nombre = form.cleaned_data.get("nombre")
+		send_mail(asunto,
+			mensaje_email,
+			email_from,
+			[email_to],
+			fail_silently=False
+			)
+
+	context = {
+		"form": form,
+	}
+
+	return render(request,"forms.html",context)
